9780b622a41f0c02924ec6a5e74d60864cd973ae
Heidi Sokolovski <heidi.sokolovski@gmail.com>
2022-10-30 21:40:03 +0100

    added admin site and sport tables

--- web2_lab1/src/webapp.ts
+++ web2_lab1/src/webapp.ts
@@ -1,9 +1,10 @@
 import express from 'express';
-import fs from 'fs';
+import fs, {open} from 'fs';
 import path from 'path'
 import https from 'https';
-import { auth, requiresAuth } from 'express-openid-connect';
+import {auth, requiresAuth} from 'express-openid-connect';
 import dotenv from 'dotenv'
+import * as data from './data.json';
 dotenv.config()
 
 const app = express();
@@ -11,52 +12,64 @@ app.set("views", path.join(__dirname, "views"));
 app.set('view engine', 'pug');
 
 const port = 4080;
+const admin = "admin"
 
 const config = {
-  authRequired : false,
-  idpLogout : true, //login not only from the app, but also from identity provider
-  secret: 'proizvoljno',
-  baseURL: `https://localhost:${port}`,
-  clientID: 'g6rdQD7fL1mWB106rr34IoulMRihesB4',
-  issuerBaseURL: 'https://dev-a1hoxpqf7enjxxm0.us.auth0.com',
-  clientSecret: 'wzPxhWC2R5ywVSvzYWEHz0jwIdU5zhAivEU7SQwowzQie85SelqTci6BbBUo9djP',
-  authorizationParams: {
-    response_type: 'code' ,
-    //scope: "openid profile email"
-   },
+    authRequired: false,
+    idpLogout: true, //login not only from the app, but also from identity provider
+    secret: 'proizvoljno',
+    baseURL: `https://localhost:${port}`,
+    clientID: 'g6rdQD7fL1mWB106rr34IoulMRihesB4',
+    issuerBaseURL: 'https://dev-a1hoxpqf7enjxxm0.us.auth0.com',
+    clientSecret: 'wzPxhWC2R5ywVSvzYWEHz0jwIdU5zhAivEU7SQwowzQie85SelqTci6BbBUo9djP',
+    authorizationParams: {
+        response_type: 'code',
+        //scope: "openid profile email"
+    },
 };
 // auth router attaches /login, /logout, and /callback routes to the baseURL
 app.use(auth(config));
 
-app.get('/',  function (req, res) {
-  let username : string | undefined;
-  if (req.oidc.isAuthenticated()) {
-    username = req.oidc.user?.name ?? req.oidc.user?.sub;
-  }
-  res.render('index', {username});
+app.get('/', function (req, res) {
+    let username: string | undefined;
+    if (req.oidc.isAuthenticated()) {
+        console.log("AUTH YES")
+        username = req.oidc.user?.nickname ?? req.oidc.user?.sub;
+    }
+    res.render('index', {username, data});
 });
 
 app.get('/private', requiresAuth(), function (req, res) {
-    const user = JSON.stringify(req.oidc.user);
-    res.render('private', {user});
+    const username = req.oidc.user?.nickname;
+    if (username == admin) {
+        res.render('admin', {username, data});
+    } else {
+        res.render('private', {username, data});
+    }
 });
 
 app.get("/sign-up", (req, res) => {
-  res.oidc.login({
-    returnTo: '/',
-    authorizationParams: {
-      screen_hint: "signup",
-    },
-  });
+    res.oidc.login({
+        returnTo: '/',
+        authorizationParams: {
+            screen_hint: "signup",
+        },
+    });
+});
+
+app.get("/logout", (req, res) => {
+    res.oidc.logout({
+        returnTo: '/'
+    })
 });
 
 https.createServer({
     key: fs.readFileSync('server.key'),
     cert: fs.readFileSync('server.cert')
-  }, app)
-  .listen(port, function () {
-    console.log(`Server running at https://localhost:${port}/`);
-  });
+}, app)
+    .listen(port, function () {
+        console.log(`Server running at https://localhost:${port}/`);
+    });
 
 /*
 const { requiresAuth } = require('express-openid-connect');
